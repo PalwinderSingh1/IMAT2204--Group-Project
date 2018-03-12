2dc9720a960fe4a1bb8da1f5e420868963a75673
Palwinder <36229806+PalwinderSingh1@users.noreply.github.com>
2018-03-12 13:41:02 +0000

    Add files via upload

---
+++ MVCFilms/Models/clsFilm.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCFilms.Models
+{
+    public class clsFilm
+    {
+        public string Headline { get; set; }
+        public string ByLine { get; set; }
+        public string Content { get; set; }
+
+    }
+}
